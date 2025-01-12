47f21225931da237925a8e2b8e3bc77d15fd26d1
porshe90 <porshe90@bk.ru>
2025-01-12 19:14:11 +0200

    test

--- lesson_07/classwork/08.py
+++ lesson_07/classwork/08.py
@@ -1,6 +1,6 @@
 numbers = (2, 4, 6, 8, 10)
 word = "hello"
 result = (''.join(c * 2 for c in word), sum(numbers[:3]),
-          word[0].upper() + word[-1].upper())
+          word[0].upper() +word[1:-1]+ word[-1].upper())
 print(result)
 
