f05c4eeac732670c9ac9025e47054a6a7599ac19
porshe90 <porshe90@bk.ru>
2025-01-29 19:01:43 +0200

    homewrork 11 update naming and import

--- lesson_011/home_task/main.py
+++ lesson_011/home_task/main.py
@@ -1,4 +1,4 @@
-import AnimalBase
+from animal_base import Lion,Elephant,Parrot,Eagle
 def zoo_announcement(animals):
     for animal in animals:
         print(f"Meet {animal.name}, age {animal.age}. They say:")
@@ -7,10 +7,10 @@ def zoo_announcement(animals):
 
 
 if __name__ == "__main__":
-    lion = AnimalBase.Lion("Simba", 5)
-    elephant = AnimalBase.Elephant("Dumbo", 3)
-    parrot = AnimalBase.Parrot("Polly", 2)
-    eagle = AnimalBase.Eagle("Eddie", 4)
+    lion = Lion("Simba", 5)
+    elephant = Elephant("Dumbo", 3)
+    parrot = Parrot("Polly", 2)
+    eagle = Eagle("Eddie", 4)
 
     animals_in_zoo = [lion, elephant, parrot, eagle]
     zoo_announcement(animals_in_zoo)
